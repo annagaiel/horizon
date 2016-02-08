7d1441ed06c1034db301dd7358fcce5130a6b5bf
annagaiel <annagaiel@gmail.com>
2016-02-07 17:05:25 -0800

    Add oo-marathon

---
+++ oo-marathon/lib/zoo.rb
@@ -0,0 +1,51 @@
+require_relative 'cage'
+require_relative 'employee'
+
+class Zoo
+  ZooAtCapacity = "Zoo is at capacity."
+  attr_reader :season_opening_date, :season_closing_date, :cages, :employees
+  def initialize(name, season_opening_date, season_closing_date)
+    @name = name
+    @season_opening_date = season_opening_date
+    @season_closing_date = season_closing_date
+    @cages = []
+    10.times do
+      @cages << Cage.new
+    end
+    @employees = []
+  end
+
+  def add_employee(employee)
+    employees << employee
+  end
+
+  def open?(date)
+    date >= season_opening_date && date < season_closing_date
+  end
+
+  def add_animal(animal)
+    reached_capacity = true
+    cages.each do |cage|
+      if cage.empty?
+        cage.animal = animal
+        reached_capacity = false
+        break
+      end
+    end
+    raise ZooAtCapacity if reached_capacity
+  end
+
+  def visit
+    noise = ''
+    employees.each do |employee|
+      noise += employee.greet + "\n"
+    end
+    cages.each do |cage|
+      if !cage.empty?
+        noise += cage.animal.speak + "\n"
+      end
+    end
+    noise
+  end
+  
+end
